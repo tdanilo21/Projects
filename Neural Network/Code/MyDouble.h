53956f66a427e2afa5debe1d501f01e6b74be9aa
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2021-06-18 13:31:59 +0200

    Create MyDouble.h

---
+++ Neural Network/Code/MyDouble.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <vector>
+
+using namespace std;
+
+class MyDouble {
+public:
+	double val;
+
+	MyDouble(double val = 0);
+	MyDouble(const MyDouble& other);
+
+	static MyDouble& Random();
+	static MyDouble& DotProduct(const vector<MyDouble>& a, const vector<MyDouble>& b);
+
+	MyDouble& operator=(const MyDouble& other);
+	MyDouble& operator+=(const MyDouble& other);
+	MyDouble& operator*(const MyDouble& other) const;
+	MyDouble& operator+(const MyDouble& other) const;
+	MyDouble& operator-(const MyDouble& other) const;
+};
