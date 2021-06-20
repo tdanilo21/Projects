b9f9e0741878cf67b155ca6e43580996407f28bf
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2021-06-21 00:22:35 +0200

    Update MyDouble.h

--- Neural Network/Code/MyDouble.h
+++ Neural Network/Code/MyDouble.h
@@ -10,12 +10,12 @@ public:
 	MyDouble(double val = 0);
 	MyDouble(const MyDouble& other);
 
-	static MyDouble& Random();
-	static MyDouble& DotProduct(const vector<MyDouble>& a, const vector<MyDouble>& b);
+	static MyDouble Random();
+	static MyDouble DotProduct(const vector<MyDouble>& a, const vector<MyDouble>& b);
 
-	MyDouble& operator=(const MyDouble& other);
-	MyDouble& operator+=(const MyDouble& other);
-	MyDouble& operator*(const MyDouble& other) const;
-	MyDouble& operator+(const MyDouble& other) const;
-	MyDouble& operator-(const MyDouble& other) const;
+	MyDouble operator=(const MyDouble& other);
+	MyDouble operator+=(const MyDouble& other);
+	MyDouble operator*(const MyDouble& other) const;
+	MyDouble operator+(const MyDouble& other) const;
+	MyDouble operator-(const MyDouble& other) const;
 };
