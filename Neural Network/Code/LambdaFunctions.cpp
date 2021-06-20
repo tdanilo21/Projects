836c2491c893dba1be977aa72eb387b19af7e218
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2021-06-21 00:23:24 +0200

    Create LambdaFunctions.cpp

---
+++ Neural Network/Code/LambdaFunctions.cpp
@@ -0,0 +1,11 @@
+#include "LambdaFunctions.h"
+
+using namespace std;
+
+// Activation function is currently sigmoid.
+MyDouble ActivationFunction(const MyDouble& x) { return *new MyDouble(tanh(x.val)); }
+MyDouble ActivationFunctionDerivative(const MyDouble& x) { return MyDouble(1.0f - x.val * x.val); }
+
+MyDouble Multiply(const MyDouble& a, const MyDouble& b) { return a * b; }
+MyDouble Add(const MyDouble& a, const MyDouble& b) { return a + b; }
+MyDouble Subtract(const MyDouble& a, const MyDouble& b) { return a - b; }
