5c35d134f791b577838a6898f342fb06b02c2586
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2021-06-22 00:30:29 +0200

    Update MyDouble.cpp

--- Neural Network/Code/MyDouble.cpp
+++ Neural Network/Code/MyDouble.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 MyDouble::MyDouble(double val) { this->val = val; }
 MyDouble::MyDouble(const MyDouble& other) { this->val = other.val; }
-MyDouble MyDouble::Random() { return *new MyDouble(((double)rand() / (double)RAND_MAX) * 2 - 1); }
+MyDouble MyDouble::Random() { return MyDouble(((double)rand() / (double)RAND_MAX) * 2 - 1); }
 MyDouble MyDouble::DotProduct(const vector<MyDouble>& a, const vector<MyDouble>& b) {
 	assert(a.size() == b.size());
-	MyDouble result = *new MyDouble();
+	MyDouble result = MyDouble();
 	for (int i = 0; i < a.size(); i++)
 		result += a[i] * b[i];
 	return result;
 }
 MyDouble MyDouble::operator=(const MyDouble& other) { this->val = other.val; return *this; }
 MyDouble MyDouble::operator+=(const MyDouble& other) { this->val += other.val; return *this; }
-MyDouble MyDouble::operator*(const MyDouble& other) const { return *new MyDouble(this->val * other.val); }
-MyDouble MyDouble::operator+(const MyDouble& other) const { return *new MyDouble(this->val + other.val); }
-MyDouble MyDouble::operator-(const MyDouble& other) const { return *new MyDouble(this->val - other.val); }
+MyDouble MyDouble::operator*(const MyDouble& other) const { return MyDouble(this->val * other.val); }
+MyDouble MyDouble::operator+(const MyDouble& other) const { return MyDouble(this->val + other.val); }
+MyDouble MyDouble::operator-(const MyDouble& other) const { return MyDouble(this->val - other.val); }
