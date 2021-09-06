b29d5a3ceae27b6cf6f226c9f58420a5a9314d63
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2021-09-06 13:45:30 +0200

    Update Matrix.cs

--- Matrix/Code/Matrix.cs
+++ Matrix/Code/Matrix.cs
@@ -10,60 +10,86 @@ namespace ConsoleApp1
         #region Abstract
         public abstract void Random();
         protected abstract T OperatorMultiply(T a);
+        protected abstract T OperatorDevide(T a);
         protected abstract T OperatorAdd(T a);
+        protected abstract T OperatorSubtract(T a);
         #endregion
         #region Operators
         public static T operator *(T a, AbstractElement<T> b)
         {
             return a.OperatorMultiply((T)b);
         }
-        public static T operator+(T a, AbstractElement<T> b)
+        public static T operator /(T a, AbstractElement<T> b)
+        {
+            return a.OperatorDevide((T)b);
+        }
+        public static T operator +(T a, AbstractElement<T> b)
         {
             return a.OperatorAdd((T)b);
         }
+        public static T operator -(T a, AbstractElement<T> b)
+        {
+            return a.OperatorSubtract((T)b);
+        }
         #endregion
     }
-    class MyInt : AbstractElement<MyInt>
+    class MyDouble : AbstractElement<MyDouble>
     {
         private static Random rand = new Random();
-        public int val = 0;
+        public double val;
 
-        #region Operators
-        public MyInt() { }
-        public MyInt(int new_val) { this.val = new_val; }
-        public MyInt(MyInt a) { this.val = a.val; }
+        #region Constructors
+        public MyDouble()
+        {
+            this.val = 0;
+        }
+        public MyDouble(double new_val)
+        {
+            this.val = new_val;
+        }
+        public MyDouble(MyDouble a)
+        {
+            this.val = a.val;
+        }
         #endregion
         #region Funcitons
         public override void Random()
         {
-            this.val = MyInt.rand.Next();
+            this.val = MyDouble.rand.Next();
         }
-        protected override MyInt OperatorMultiply(MyInt a)
+        protected override MyDouble OperatorMultiply(MyDouble a)
         {
-            return new MyInt(this.val * a.val);
+            return new MyDouble(this.val * a.val);
         }
-        protected override MyInt OperatorAdd(MyInt a)
+        protected override MyDouble OperatorDevide(MyDouble a)
         {
-            return new MyInt(this.val + a.val);
+            return new MyDouble(this.val / a.val);
+        }
+        protected override MyDouble OperatorAdd(MyDouble a)
+        {
+            return new MyDouble(this.val + a.val);
+        }
+        protected override MyDouble OperatorSubtract(MyDouble a)
+        {
+            return new MyDouble(this.val - a.val);
         }
         #endregion
     }
     class Matrix<T> where T : AbstractElement<T>, new()
     {
-
         private T[,] data;
-        private uint rows, cols;
+        private int rows, cols;
 
         #region Constructors
         public Matrix()
         {
             Construct(1, 1, new T());
         }
-        public Matrix(uint rows, uint cols)
+        public Matrix(int rows, int cols)
         {
             Construct(rows, cols, new T());
         }
-        public Matrix(uint rows, uint cols, T defaultData)
+        public Matrix(int rows, int cols, T defaultData)
         {
             Construct(rows, cols, defaultData);
         }
@@ -74,12 +100,12 @@ namespace ConsoleApp1
         }
         #endregion
         #region Helper function
-        private void Construct(uint rows, uint cols, T defaultData)
+        private void Construct(int rows, int cols, T defaultData)
         {
             Resize(rows, cols);
             Fill(defaultData);
         }
-        public void Resize(uint rows = 1, uint cols = 1)
+        public void Resize(int rows = 1, int cols = 1)
         {
             this.rows = rows;
             this.cols = cols;
@@ -91,50 +117,50 @@ namespace ConsoleApp1
         }
         public void Fill(T data)
         {
-            for (uint i = 0; i < this.rows; i++)
-                for (uint j = 0; j < this.cols; j++)
+            for (int i = 0; i < this.rows; i++)
+                for (int j = 0; j < this.cols; j++)
                     this.data[i, j] = data;
         }
         public void SetData(T[,] data)
         {
-            for (uint i = 0; i < this.rows; i++)
-                for (uint j = 0; j < this.cols; j++)
+            for (int i = 0; i < this.rows; i++)
+                for (int j = 0; j < this.cols; j++)
                     this.data[i, j] = data[i, j];
         }
         public void Randomize()
         {
-            for (uint i = 0; i < this.rows; i++)
-                for (uint j = 0; j < this.cols; j++)
+            for (int i = 0; i < this.rows; i++)
+                for (int j = 0; j < this.cols; j++)
                     this.data[i, j].Random();
         }
-        public T[] GetRow(uint index)
+        public T[] GetRow(int index)
         {
             Debug.Assert(index < this.rows);
             T[] result = new T[this.cols];
-            for (uint i = 0; i < this.cols; i++)
+            for (int i = 0; i < this.cols; i++)
                 result[i] = this.data[index, i];
             return result;
         }
-        public T[] GetCol(uint index)
+        public T[] GetCol(int index)
         {
             Debug.Assert(index < this.cols);
             T[] result = new T[this.rows];
-            for (uint i = 0; i < this.rows; i++)
+            for (int i = 0; i < this.rows; i++)
                 result[i] = this.data[i, index];
             return result;
         }
         public static T[] ToArray(Matrix<T> m)
         {
             T[] result = new T[m.rows * m.cols];
-            for (uint i = 0; i < m.rows; i++)
-                for (uint j = 0; j < m.cols; j++)
+            for (int i = 0; i < m.rows; i++)
+                for (int j = 0; j < m.cols; j++)
                     result[i * m.cols + j] = m.data[i, j];
             return result;
         }
         public static Matrix<T> FromArray(T[] arr)
         {
-            Matrix<T> result = new Matrix<T>(Convert.ToUInt32(arr.Length), 1);
-            for (uint i = 0; i < arr.Length; i++)
+            Matrix<T> result = new Matrix<T>(arr.Length, 1);
+            for (int i = 0; i < arr.Length; i++)
                 result.data[i, 1] = arr[i];
             return result;
         }
@@ -144,13 +170,13 @@ namespace ConsoleApp1
         {
             Debug.Assert(a.cols == b.rows);
             Matrix<T> result = new Matrix<T>(a.rows, b.cols);
-            for (uint i = 0; i < a.rows; i++)
+            for (int i = 0; i < a.rows; i++)
             {
-                for (uint j = 0; j < b.cols; j++)
+                for (int j = 0; j < b.cols; j++)
                 {
                     T sum = new T();
                     T[] r = a.GetRow(i), c = b.GetCol(j);
-                    for (uint t = 0; t < a.cols; t++)
+                    for (int t = 0; t < a.cols; t++)
                         sum += r[t] * c[t];
                     result.data[i, j] = sum;
                 }
@@ -160,8 +186,8 @@ namespace ConsoleApp1
         public static Matrix<T> Transpose(Matrix<T> m)
         {
             Matrix<T> result = new Matrix<T>(m.cols, m.rows);
-            for (uint i = 0; i < m.rows; i++)
-                for (uint j = 0; j < m.cols; j++)
+            for (int i = 0; i < m.rows; i++)
+                for (int j = 0; j < m.cols; j++)
                     result.data[j, i] = m.data[i, j];
             return result;
         }
@@ -169,33 +195,149 @@ namespace ConsoleApp1
         {
             Debug.Assert(a.rows == b.rows && a.cols == b.cols);
             Matrix<T> result = new Matrix<T>(a.rows, a.cols);
-            for (uint i = 0; i < a.rows; i++)
-                for (uint j = 0; j < a.cols; j++)
+            for (int i = 0; i < a.rows; i++)
+                for (int j = 0; j < a.cols; j++)
                     result.data[i, j] = func(a.data[i, j], b.data[i, j]);
             return result;
         }
         public void Scale(Func<T, T> func)
         {
-            for (uint i = 0; i < this.rows; i++)
-                for (uint j = 0; j < this.cols; j++)
+            for (int i = 0; i < this.rows; i++)
+                for (int j = 0; j < this.cols; j++)
                     this.data[i, j] = func(this.data[i, j]);
         }
         #endregion
 
         public struct Dims
         {
-            public uint rows, cols;
-            public Dims(uint rows = 1, uint cols = 1) { this.rows = rows; this.cols = cols; }
+            public int rows, cols;
+            public Dims(int rows = 1, int cols = 1) { this.rows = rows; this.cols = cols; }
+        }
+    }
+    class LambdaFunctions
+    {
+        // Activation function is currently sigmoid
+        public static Func<MyDouble, MyDouble> ActivationFunction = x => { return new MyDouble(Math.Tanh(x.val)); };
+        public static Func<MyDouble, MyDouble> ActivationFunctionDerivative = x => { return new MyDouble(1 - x.val * x.val); };
+
+        public static Func<MyDouble, MyDouble, MyDouble> Multiply = (x, y) => { return new MyDouble(x * y); };
+        public static Func<MyDouble, MyDouble, MyDouble> Devide = (x, y) => { return new MyDouble(x / y); };
+        public static Func<MyDouble, MyDouble, MyDouble> Add = (x, y) => { return new MyDouble(x + y); };
+        public static Func<MyDouble, MyDouble, MyDouble> Subtract = (x, y) => { return new MyDouble(x - y); };
+    }
+    class Layer
+    {
+        private int size, next_size;
+        private Matrix<MyDouble> values, weights, bias;
+
+        #region Constructors
+        public Layer()
+        {
+            this.size = this.next_size = 0;
+        }
+        public Layer(int size, int next_size)
+        {
+            Resize(size, next_size);
+        }
+        #endregion
+        #region Getters and Setters
+        public void Resize(int size, int next_size)
+        {
+            this.size = size;
+            this.next_size = next_size;
+            this.values = new Matrix<MyDouble>(size, 1);
+            this.weights = new Matrix<MyDouble>(size, next_size);
+            this.weights.Randomize();
+            this.bias = new Matrix<MyDouble>(next_size, 1);
+            this.bias.Randomize();
+        }
+        public int Size()
+        {
+            return this.size;
+        }
+        public void SetValues(Matrix<MyDouble> new_values)
+        {
+            Debug.Assert(new_values.Dimensions().rows == this.size && new_values.Dimensions().cols == 1);
+            this.values = new Matrix<MyDouble>(new_values);
+        }
+        public Matrix<MyDouble> GetValues()
+        {
+            return new Matrix<MyDouble>(this.values);
+        }
+        public void SetWeights(Matrix<MyDouble> new_weights)
+        {
+            Debug.Assert(new_weights.Dimensions().rows == this.size && new_weights.Dimensions().cols == this.next_size);
+            this.weights = new Matrix<MyDouble>(new_weights);
+        }
+        public Matrix<MyDouble> GetWeights()
+        {
+            return new Matrix<MyDouble>(this.weights);
+        }
+        public void SetBias(Matrix<MyDouble> new_bias)
+        {
+            Debug.Assert(new_bias.Dimensions().rows == this.next_size && new_bias.Dimensions().cols == 1);
+            this.bias = new Matrix<MyDouble>(new_bias);
+        }
+        public Matrix<MyDouble> GetBias()
+        {
+            return new Matrix<MyDouble>(this.bias);
+        }
+        #endregion
+        #region Funcitons
+        public Matrix<MyDouble> Propagate()
+        {
+            Matrix<MyDouble> weights_t = Matrix<MyDouble>.Transpose(this.weights);
+            Matrix<MyDouble> next_values = Matrix<MyDouble>.Product(weights_t, this.values);
+            next_values = Matrix<MyDouble>.ElementWise(next_values, this.bias, LambdaFunctions.Add);
+            next_values.Scale(LambdaFunctions.ActivationFunction);
+            return next_values;
+        }
+        public void Reset()
+        {
+            this.values.Fill(new MyDouble());
+        }
+        #endregion
+    }
+    class NeuralNetwork
+    {
+        private int size;
+        private Layer[] layers;
+        private Matrix<MyDouble> maxInput, maxOutput;
+
+        public NeuralNetwork() { }
+        public NeuralNetwork(int size, int[] layout, double[] maxInputData, double[] maxOutputData)
+        {
+
+        }
+
+        private void Resize(int size)
+        {
+            this.size = size;
+            this.layers = new Layer[size];
+        }
+        private void Setlayout(int[] layout)
+        {
+            for (int i = 0; i < layout.Length; i++)
+            {
+                int next_size = (i == layout.Length - 1 ? 0 : layout[i + 1]);
+                this.layers[i] = new Layer(layout[i], next_size);
+            }
+        }
+        private void SetMaxData(double[] maxInputData, double[] maxOutputData)
+        {
+            Debug.Assert(this.layers[0].Size() == maxInputData.Length);
+            Debug.Assert(this.layers[this.size - 1].Size() == maxOutputData.Length);
+            this.maxInput = new Matrix<MyDouble>(maxInputData.Length, 1);
         }
     }
     class Program
     {
-        static void Print(Matrix<MyInt> m)
+        static void Print(Matrix<MyDouble> m)
         {
-            for (uint i = 0; i < m.Dimensions().rows; i++)
+            for (int i = 0; i < m.Dimensions().rows; i++)
             {
-                MyInt[] row = m.GetRow(i);
-                for (uint j = 0; j < m.Dimensions().cols; j++)
+                MyDouble[] row = m.GetRow(i);
+                for (int j = 0; j < m.Dimensions().cols; j++)
                     Console.Write($"{row[j].val} ");
                 Console.WriteLine();
             }
@@ -203,25 +345,24 @@ namespace ConsoleApp1
         }
         static void Main(string[] args)
         {
-            Matrix<MyInt> m = new Matrix<MyInt>(2, 3);
-            m.Scale(x => new MyInt(2 + x.val));
+            Matrix<MyDouble> m = new Matrix<MyDouble>(2, 3);
+            m.Scale(x => new MyDouble(2 + x.val));
             Print(m);
-            m.Scale(x => new MyInt(3 * x.val));
+            m.Scale(x => new MyDouble(3 * x.val));
             Print(m);
-            m = Matrix<MyInt>.Transpose(m);
+            m = Matrix<MyDouble>.Transpose(m);
             Print(m);
 
-            Matrix<MyInt> a = new Matrix<MyInt>(2, 3, new MyInt(2));
-            Matrix<MyInt> b = new Matrix<MyInt>(3, 3, new MyInt(3));
-            Matrix<MyInt> p = Matrix<MyInt>.Product(a, b);
+            Matrix<MyDouble> a = new Matrix<MyDouble>(2, 3, new MyDouble(2));
+            Matrix<MyDouble> b = new Matrix<MyDouble>(3, 3, new MyDouble(3));
+            Matrix<MyDouble> p = Matrix<MyDouble>.Product(a, b);
             Print(p);
 
-            Func<MyInt, MyInt, MyInt> Multiply = (x, y) => { return new MyInt(x.val * y.val); };
-            Matrix<MyInt> c = new Matrix<MyInt>(3, 2, new MyInt(5));
-            Matrix<MyInt> e = Matrix<MyInt>.ElementWise(m, c, Multiply);
-            Print(e);
+            Matrix<MyDouble> c = new Matrix<MyDouble>(3, 2, new MyDouble(5));
+            Matrix<MyDouble> e = Matrix<MyDouble>.ElementWise(m, c, LambdaFunctions.Multiply);
+            Print(m);
 
-            Matrix<MyInt> r = new Matrix<MyInt>(2, 2);
+            Matrix<MyDouble> r = new Matrix<MyDouble>(2, 2);
             r.Randomize();
             Print(r);
         }
